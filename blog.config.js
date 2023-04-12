abee4c51de57ef842f4e049e01a18fa0ee8541f6
eruyu <90751933+eruyu@users.noreply.github.com>
2023-04-12 16:09:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: '深海鱼',
+  author: '深海鱼',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
